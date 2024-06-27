c6f992926d9d3d75a7fbbc64232c7bab77044c24
root <root@ROG-STRIX>
2024-06-27 11:37:33 +0530

    canvas4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,59 +1,55 @@
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
-import { ComputersCanvas } from "./canvas";
+
 import { styles } from "../styles";
+import { ComputersCanvas, EarthCanvas } from "./canvas";
 
 const Hero = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    // Check if the screen width matches a mobile device
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
-    setIsMobile(mediaQuery.matches);
-
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add listener for changes in media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Clean up listener on unmount
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
-
   return (
-    <section className="relative w-full h-screen mx-auto bg-black">
-      <div className={`absolute inset-0 ${styles.paddingX} flex flex-row items-start gap-5`}>
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-1 sm:h-80 h-40 bg-gradient-to-b from-violet to-purple" />
+        <div className="w-1 sm:h-80 h-40 violet-gradient" />  
           <div className="w-5 h-5 rounded-full bg-[#07ff30]" />
+          
         </div>
+
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#29f37d]">Arun</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white`}>
-            I am a Full Stack Developer, I build user interfaces and web applications.
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am Full Stack Developer,I build user{" "}
+            <br className="sm:block hidden" />
+            interfaces and web applications.
           </p>
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-12 h-12 rounded-full border-4 border-secondary flex justify-center items-center">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
-              animate={{ y: [0, 24, 0] }}
-              transition={{ duration: 1.5, repeat: Infinity, repeatType: "loop" }}
-              className="w-3 h-3 rounded-full bg-secondary"
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
       </div>
+     
+
+      
     </section>
-  );
+
+);
 };
 
 export default Hero;
